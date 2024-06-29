19e40039ca73993bca4596f7aed3a61a78bd3b2a
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-06-29 08:30:40 +0200

    ElapsedMillis: added assignUnsignedLong, equals and compare

--- include/register_elapsedMillis.hpp
+++ include/register_elapsedMillis.hpp
@@ -30,10 +30,44 @@ namespace asElapsedMillis
     out_pointer->SetReturnAddress(self);
   }
 
+  void assignUnsignedLong(asIScriptGeneric* out_pointer)
+  {
+    unsigned long rightOperand = out_pointer->GetArgDWord(0);
+    elapsedMillis* self = static_cast<elapsedMillis*>(out_pointer->GetObject());
+    *self = rightOperand;
+    out_pointer->SetReturnAddress(self);
+  }
+
   void opImplConv(asIScriptGeneric* out_pointer)
   {
     unsigned long value = *static_cast<elapsedMillis*>(out_pointer->GetObject());
     out_pointer->SetReturnDWord(value);
+  }
+
+  void equals(asIScriptGeneric* in_generic)
+  {
+    elapsedMillis* a = static_cast<elapsedMillis*>(in_generic->GetObject());
+    elapsedMillis* b = static_cast<elapsedMillis*>(in_generic->GetArgAddress(0));
+    *reinterpret_cast<bool*>(in_generic->GetAddressOfReturnLocation()) = (*a == *b);
+  }
+
+  void compare(asIScriptGeneric* in_generic)
+  {
+    elapsedMillis* a = static_cast<elapsedMillis*>(in_generic->GetObject());
+    elapsedMillis* b = static_cast<elapsedMillis*>(in_generic->GetArgAddress(0));
+
+    int result = 0;
+
+    if (*a < *b)
+    {
+      result = -1;
+    }
+    else if (*a > *b)
+    {
+      result = 1;
+    }
+
+    *static_cast<int*>(in_generic->GetAddressOfReturnLocation()) = result;
   }
 
   void registerElapsedMillis(asIScriptEngine* io_engine)
@@ -42,11 +76,12 @@ namespace asElapsedMillis
     resultCode = io_engine->RegisterObjectBehaviour("ElapsedMillis", asBEHAVE_CONSTRUCT, "void f()", asFUNCTION(Constructor), asCALL_GENERIC); assert( resultCode >= 0 );
     resultCode = io_engine->RegisterObjectBehaviour("ElapsedMillis", asBEHAVE_CONSTRUCT,  "void f(const ElapsedMillis& in)", asFUNCTION(CopyConstructor), asCALL_GENERIC); assert(resultCode >= 0);
     resultCode = io_engine->RegisterObjectBehaviour("ElapsedMillis", asBEHAVE_DESTRUCT, "void f()", asFUNCTION(Destructor), asCALL_GENERIC); assert( resultCode >= 0 );
-    resultCode = io_engine->RegisterObjectMethod("ElapsedMillis", "ElapsedMillis& opAssign(const ElapsedMillis &in)", asFUNCTION(assign), asCALL_GENERIC); assert(resultCode >= 0);
+    resultCode = io_engine->RegisterObjectMethod("ElapsedMillis", "ElapsedMillis& opAssign(const ElapsedMillis& in)", asFUNCTION(assign), asCALL_GENERIC); assert(resultCode >= 0);
+    resultCode = io_engine->RegisterObjectMethod("ElapsedMillis", "ElapsedMillis& opAssign(uint& in)", asFUNCTION(assignUnsignedLong), asCALL_GENERIC); assert(resultCode >= 0);
     
     resultCode = io_engine->RegisterObjectMethod("ElapsedMillis", "uint opImplConv()", asFUNCTION(opImplConv), asCALL_GENERIC); assert(resultCode >= 0);
-    // ToDo: equals
-    // ToDo: compare
+    resultCode = io_engine->RegisterObjectMethod("ElapsedMillis", "bool opEquals(const ElapsedMillis& in) const", asFUNCTION(equals), asCALL_GENERIC); assert(resultCode >= 0);
+    resultCode = io_engine->RegisterObjectMethod("ElapsedMillis", "int opCmp(const ElapsedMillis& in) const", asFUNCTION(compare), asCALL_GENERIC); assert(resultCode >= 0);
   }
 }
 
